8dc1906611465f5f821779a9687fd7b3a9083417
Evrins Hu <evrins.hu@gmail.com>
2024-05-07 17:47:56 +0800

    chore: upgrade gopsutil to v3

--- internal/core/statsapi.go
+++ internal/core/statsapi.go
@@ -7,10 +7,10 @@ import (
 
 	"github.com/pbnjay/memory"
 
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/disk"
-	"github.com/shirou/gopsutil/host"
-	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 type machInfo struct {
