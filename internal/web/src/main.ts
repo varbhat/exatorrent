04569d62a9c1c6b110fe20882e00814313dd401d
Evrins Hu <evrins.hu@gmail.com>
2025-01-28 17:30:02 +0800

    refactor: rename index.css to app.css Index.svelte to App.svelte

--- internal/web/src/main.ts
+++ internal/web/src/main.ts
@@ -1,7 +1,7 @@
-import './index.css';
-import Index from './Index.svelte';
+import './app.css';
+import App from './App.svelte';
 import { mount } from 'svelte';
 
-const app = mount(Index, { target: document.getElementById('app')! });
+const app = mount(App, { target: document.getElementById('app')! });
 
 export default app;
